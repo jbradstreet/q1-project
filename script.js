d5b4b4a66b563e2a7e23240345409864112b0ba4
Jenni Bradstreet <jennic15@gmail.com>
2016-03-09 10:18:19 -0800

    Begin Add It button functionality

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     userInput = $('#searchIt').val();
     console.log(userInput);
 
-    // make container for data visible !!!!
+    // make container for episode data visible !!!!
     $('#displayIt').css('visibility', 'visible');
 
     $.ajax({
@@ -31,13 +31,12 @@ $(document).ready(function() {
 
             // make framework for data outpout
             var $newRow = $('.row.template').clone().removeClass('template');
-
-
             var $imageOutput = $newRow.find('.episodeImage');
-            console.log(value)
+
             $imageOutput.append($('<img src=' + value.image.medium + '>'));
 
             var  $episodeOutput = $newRow.find('.episodeDetails');
+
             $episodeOutput.append($('<h3>' + value.name + '</h3>'));
             $episodeOutput.append($('<h5>' + value.airdate + '</h5>'));
             $episodeOutput.append($("<button type='submit' class='addIt'>" + 'Add it!' + '</button>'));
@@ -46,6 +45,20 @@ $(document).ready(function() {
             $('#displayIt').append($newRow);
 
             });
+
+            // populate My List on "add it" click with Episode Details
+            $('#addIt').click(function(event) {
+              event.preventDefault();
+
+              var holdEpDetails = '';
+
+              
+
+            });
+
+
+
+
           }
         });
       },
