39742c39a44aaac3ac597aef0e7da5811fb63a54
Jenni Bradstreet <jennic15@gmail.com>
2016-03-11 13:05:23 -0800

    Comment out display attempt for presentation.

--- script.js
+++ script.js
@@ -93,37 +93,37 @@ $(document).ready(function() {
     $('#revealIt').css('visibility', 'visible');
 
     // work in progress - try to display saved list data when page loads
-    (function displayData() {
-
-      if (JSON.parse(localStorage.getItem('myList')) !== 'undefined' && JSON.parse(localStorage.getItem('myList')) !== null) {
-
-        var listValue = JSON.parse(localStorage.getItem('myList'));
-        console.log(listValue);
-
-        $.each(listValue, function(key, value) {
-          // build a new form template to throw myList into.
-          var $anotherForm = $('.row.template').clone().removeClass('template');
-          var $listEpisodeOutput = $anotherForm.find('.episodeDetails');
-
-          $listEpisodeOutput.append($('<h3>' + value.name + '</h3>'));
-          $listEpisodeOutput.append($('<h5>' + 'Air date:' + ' ' + value.airdate + '</h5>'));
-          $listEpisodeOutput.append($('<p>' + value.summary + '<p>'));
-
-          var $yetAnotherForm = $('.anotherTemplate.copy').clone().removeClass('copy');
-
-          $('#revealIt article').append($anotherForm);
-            var savedListData = $listEpisodeOutput.clone();
-
-            $anotherForm.find('p').append(savedListData);
-            $anotherForm.find('input').click(function() {
-              $anotherForm.remove();
-            });
-
-          console.log($yetAnotherForm);
-          return $('#revealIt article').append($yetAnotherForm);
-        });
-      }
-    })();
+    // (function displayData() {
+    //
+    //   if (JSON.parse(localStorage.getItem('myList')) !== 'undefined' && JSON.parse(localStorage.getItem('myList')) !== null) {
+    //
+    //     var listValue = JSON.parse(localStorage.getItem('myList'));
+    //     console.log(listValue);
+    //
+    //     $.each(listValue, function(key, value) {
+    //       // build a new form template to throw myList into.
+    //       var $anotherForm = $('.row.template').clone().removeClass('template');
+    //       var $listEpisodeOutput = $anotherForm.find('.episodeDetails');
+    //
+    //       $listEpisodeOutput.append($('<h3>' + value.name + '</h3>'));
+    //       $listEpisodeOutput.append($('<h5>' + 'Air date:' + ' ' + value.airdate + '</h5>'));
+    //       $listEpisodeOutput.append($('<p>' + value.summary + '<p>'));
+    //
+    //       var $yetAnotherForm = $('.anotherTemplate.copy').clone().removeClass('copy');
+    //
+    //       $('#revealIt article').append($anotherForm);
+    //         var savedListData = $listEpisodeOutput.clone();
+    //
+    //         $anotherForm.find('p').append(savedListData);
+    //         $anotherForm.find('input').click(function() {
+    //           $anotherForm.remove();
+    //         });
+    //
+    //       console.log($yetAnotherForm);
+    //       return $('#revealIt article').append($yetAnotherForm);
+    //     });
+    //   }
+    // })();
 
   });
 
